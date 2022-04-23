0b666b482f70807bb74c6e6c6deaef971e92b5c2
olayinka koleaje <koleajeolayinka@gmail.com>
2022-04-23 07:24:30 +0100

    term

--- src/term.java
+++ src/term.java
@@ -2,12 +2,29 @@ public class term {
     public static void main(String[] args) {
 
         System.out.println("\uD83D\uDE00");
-        for (int i = 1; i <12 ; i++) {
-            for (int j = 2; j < 10; j++) {
-                System.out.printf("%d x %d = " + i,j + i * j);
-                //System.out.printf();
-            }
+//        for (int i = 1; i <12 ; i++) {
+//            for (int j = 2; j < 10; j++) {
+//                System.out.printf("%d x %d = " + i,j + i * j);
+//                //System.out.printf();
+//            }
+//
+//        }
 
+                /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int num = 1;
+                for (int i = 1; i <= num ; i++) {
+
+                        System.out.println(num + " Hello world");
+                        System.out.println((num+num) + " I am a file");
+                        System.out.println((num+num+num) + " Read me until end-of-file");
+                    num = num + 1;
+                    break;
+
+                    }
+
+
+
+
+            }
         }
-    }
-}
+
