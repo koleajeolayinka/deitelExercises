4abaaa7c34c841774d19e8905c32cc2e6f6fb6a6
olayinka koleaje <koleajeolayinka@gmail.com>
2022-04-23 07:46:41 +0100

    triangle

--- src/triangle.java
+++ src/triangle.java
@@ -1,28 +1,62 @@
 public class triangle {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 4; j++) {
+        for (int i = 12; i >= 0; i--) {
+            for (int j = i; j >= 1; j--) {
 
                 System.out.print(" * ");
 
 
             }
-
             System.out.println();
 
         }
-        for (int i = 0; i < 3; i++) {
-            System.out.print("*");
+
+
+// square
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println();
         }
-            for (int j = 0; j < 3; j++)
 
-                System.out.print("1");
 
-String name = "koleaje";
-        for(;true;){
-            System.out.println(name);
+        System.out.println();
+        // coronation
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(" * ");
+        }
+            for (int j = 0; j < 5; j++) {
+                System.out.print( " # ");
+
+            }
+
+
+
+//            System.out.println();
+//
+//        }
+//        for (int i = 0; i < 3; i++) {
+//            System.out.print("*");
+//        }
+//            for (int j = 0; j < 3; j++)
+//
+//                System.out.print("1");
+//
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < ; j++) {
+//                System.out.println("*");
+
+            }
+
         }
 
-    }
+//String name = "koleaje";
+//        for(;true;){
+//            System.out.println(name);
+//        }
 
-}
+
+
+
