58c403ca74d9fb57af3a535fb9ce556a41fe7d16
olayinka koleaje <koleajeolayinka@gmail.com>
2022-04-23 07:44:27 +0100

    Test

--- src/test.java
+++ src/test.java
@@ -19,6 +19,26 @@ public class test {
                 System.out.print(">->>");
             }
         }
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            System.out.print( " * ");
+
+        }
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" *        *");
+
+        }
+        for (int i = 1; i <= 4 ; i++) {
+            System.out.print(" * ");
+
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 21; j++) {
+
+            }
+
+        }
 
     }
 }
